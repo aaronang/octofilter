7f023ff89e2d96a058e35708bfba0abb1c42300e
Aaron Ang <awz.ang@gmail.com>
2018-07-03 00:05:32 -0700

    Add Octofilter button to GitHub

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 "use strict";
 
-console.log("Hello");
+const reviewTools = document.querySelector(".pr-review-tools");
+const diffSettings = reviewTools.children[0];
+
+const carret = document.createElement("div");
+carret.classList.add("dropdown-caret", "v-align-text-bottom");
+
+const summary = document.createElement("summary");
+summary.classList.add("btn", "btn-sm");
+summary.textContent = "Octofilter settings";
+summary.appendChild(carret);
+
+const octofilterSettings = document.createElement("details");
+octofilterSettings.classList.add(
+  "diffbar-item",
+  "details-reset",
+  "details-overlay",
+  "position-relative",
+  "text-center"
+);
+octofilterSettings.appendChild(summary);
+
+reviewTools.insertBefore(octofilterSettings, diffSettings);
