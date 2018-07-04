a1d97c1c825b93242d68822eb59e0eefdf12d49d
Aaron Ang <awz.ang@gmail.com>
2018-07-03 23:50:20 -0700

    Collapse selected file extensions

--- index.js
+++ index.js
@@ -19,6 +19,8 @@ if (window.location.pathname.endsWith("files")) {
 
 function main() {
   createButton();
+  generateOptions();
+  addListeners();
 }
 
 function createButton() {
@@ -42,8 +44,8 @@ function createButton() {
   <!-- </textarea></xmp> -->
   <input name="utf8" type="hidden" value="✓">
   <h4 class="mb-2">Collapse</h4>
-  <div id="octofilter-options"></div>
-  <button class="btn btn-primary btn-sm col-12 mt-3" type="submit">Apply</button>
+  <div id="octofilter-options" style="display: grid; grid-template-columns: 1fr 1fr 1fr;"></div>
+  <button id="octofilter-btn" class="btn btn-primary btn-sm col-12 mt-3" type="submit">Apply</button>
   </div>`;
 
   const octofilterSettings = document.createElement("details");
@@ -52,10 +54,90 @@ function createButton() {
     "details-reset",
     "details-overlay",
     "position-relative",
-    "text-center"
+    "text-center",
+    "octofilter-details"
   );
   octofilterSettings.appendChild(summary);
   octofilterSettings.appendChild(popover);
 
   reviewTools.insertBefore(octofilterSettings, diffSettings);
+}
+
+function generateOptions() {
+  const extensions = getFileExtensions();
+  console.log(extensions);
+  const options = extensions
+    .map(
+      ext => `
+        <label for="octofilter-${ext}" class="text-normal">
+          <input type="checkbox" class="octofilter-ext" name="${ext}" value="${ext}" id="octofilter-${ext}">
+          <span class="commit-ref css-truncate user-select-contain expandable ">
+            <span class="css-truncate-target">${ext}</span>
+          </span>
+        </label>`
+    )
+    .join("");
+  const container = document.querySelector("#octofilter-options");
+  console.log(options);
+  container.innerHTML = options;
+}
+
+function getFileExtensions() {
+  const diffs = document.querySelectorAll(".file.js-file.js-details-container");
+
+  return [
+    ...new Set(
+      Array.from(diffs).map(node => {
+        const filepath = node.children[0].dataset.path;
+        return filepath.split(".").pop();
+      })
+    )
+  ].sort();
+}
+
+function addListeners() {
+  const config = { childList: true };
+  const callback = function(mutationsList) {
+    for (const mutation of mutationsList) {
+      if (mutation.type == "childList" && mutation.addedNodes.length > 0) {
+        generateOptions();
+      }
+    }
+  };
+  const containers = document.querySelectorAll(
+    ".js-diff-progressive-container"
+  );
+  containers.forEach(c => {
+    const observer = new MutationObserver(callback);
+    observer.observe(c, config);
+  });
+
+  const button = document.querySelector("#octofilter-btn");
+  button.addEventListener("click", handleApply);
+}
+
+function handleApply() {
+  const checkboxes = document.querySelectorAll(".octofilter-ext");
+  checkboxes.forEach(checkbox => {
+    const ext = checkbox.value;
+    if (checkbox.checked) {
+      document
+        .querySelectorAll(".file.js-file.js-details-container")
+        .forEach(node => {
+          if (node.children[0].dataset.path.split(".").pop() === ext) {
+            node.classList.add("open", "Details--on");
+          }
+        });
+    } else {
+      document
+        .querySelectorAll(".file.js-file.js-details-container")
+        .forEach(node => {
+          if (node.children[0].dataset.path.split(".").pop() === ext) {
+            node.classList.remove("open", "Details--on");
+          }
+        });
+    }
+  });
+
+  document.querySelector(".octofilter-details").open = false;
 }
